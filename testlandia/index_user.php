94e89eb03192b90a7188bc4c46d7633d9e297444
alopezdomingo <analopezdomingo@hotmail.com>
2018-06-12 13:42:16 +0200

    Actualización fuentes

--- testlandia/index_user.php
+++ testlandia/index_user.php
@@ -6,14 +6,29 @@
 	<?php include("tmpl/style.php"); ?>
 	<?php include("tmpl/sesion.php"); ?>
 </head>
-<body>
+<body style="background-color: #4f5b69">
 	<!-- MENU -->
 	<?php include("tmpl/menu_user.php"); ?>
 	<?php include("tmpl/dashboard_user.php"); ?>
-	
-		
+			<div class="col-md-9 offset-3">
+				<h1 class="text-center text-blue mtop">asdfasdf</h1>
+				<table class="table table-hover text-white">
+					<thead>
+						<tr>
+							<th>hola</th>
+							<th>hola</th>
+						</tr>
+					</thead>
+					<tr>
+						<td>adfadf</td>
+						<td>adfaf</td>
+					</tr>
+				</table>
+				<br><br><br>
+			</div>
+		</div>	
+	</div>
 	<!-- FOOTER -->
-	<br><br><br>
 	<?php include("tmpl/footer.php"); ?>
 </body>
 </html>
